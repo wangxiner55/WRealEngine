6afc12bc1e667b2643a4ec784f33fd8e8feed0ff
1468441589 <1468441589@qq.com>
2022-08-05 11:08:18 +0800

    添加项目文件。

---
+++ WRealEngine/Resoure/resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 WRealEngine.rc 使用
+//
+#define IDI_ICON1                       101
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_GITHUBTESTCANDEL_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_GITHUBTESTCANDEL			107
+#define IDI_SMALL				108
+#define IDC_GITHUBTESTCANDEL			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
